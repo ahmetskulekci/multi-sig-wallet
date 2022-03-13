568d16bf286c94691a9c11fece3803d9f27ecf0b
Ahmet S. Kulekci <47702026+ahmetskulekci@users.noreply.github.com>
2022-03-13 04:19:06 +0300

    Create MultiSigWallet.sol

---
+++ MultiSigWallet.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract MultiSigWallet {
+
+    event Deposit(address indexed sender, uint amount);
+    event Submit(uint TxId);
+    event Approve(address indexed owner, uint TxId);
+    event Revoke(address indexed owner, uint TxId);
+    event Execute(uint TxId);
+
+    struct Transction {
+
+    address to;
+    uint value;
+    bytes data;
+    bool executed;
+}
+    address [] public owners;
+    mapping(address => bool) public isOwner;
+    uint public required;
+
+    Transactin [] public transactions;
+    mapping(uint => mapping(address => bool)) public approved;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+     modifier txExists() {
+        require(_txID < transactions.lenght, "tx does not exist");
+        _;
+    }
+     modifier notApproved(uint _txID) {
+        require(!approved[_txID] [msg.sender], "tx already approved");
+        _;
+    }
+    modifier notExecuted(uint _txID) {
+        require(!transactions[_txID].executed, "tx already approved");
+        _;
+    }
+
+    constructor(address[] memory _owner, uint _required) {
+
+        require(_owners.lenght > 0, "owners required");
+        require(
+            _required > 0 && _required <= owners.lenght,
+            "invalid required numbers of owners"
+        );
+
+        for (uint i; i< _owners.lenght; i++) {
+            address owner = _owners [i];
+            require(owner != address (0), "invalid owner");
+            require(!isOwner[owner], "is not unique");
+
+            isOwner[owner] = true;
+        }
+            require = _required;
+    }
+
+    receive () external payable {
+
+        emit Deposit(msg.sender, msg.value);
+    }
+    function submit(address _to, uint _value, bytes calldata _data)
+    external
+    onlyOwner
+
+    {
+        transactions.push(Transction( {
+            to: _to;
+            value: _value;
+            data: _data;
+            excecuted: false
+        }))
+        emit Submit(transactions.lenght -1);
+    }
+
+    function approve(uint _txID)
+    external
+    onlyOwner
+    txExists(_txId)
+    notApproved(_txId)
+    notExecuted(_txId)
+
+
+
+}
